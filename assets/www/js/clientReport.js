02d014cb7fbff45d00736b14cca625d650278a71
awol2010ex <awol2010ex@gmail.com>
2012-07-26 16:25:20 +0800

    砍掉柱图展现时的动画效果

--- assets/www/js/clientReport.js
+++ assets/www/js/clientReport.js
@@ -157,10 +157,13 @@
                 items: [{
                     cls: 'column1',
                     store: store,
+                    /*
                     animate: {
                         easing: 'bounceOut',//滑动
                         duration: 750
                     },
+                    */
+                    animate:false,
                     shadow: false,
                     legend: {
                         position: {
@@ -199,8 +202,135 @@
                         	    color: '#333'//字体颜色
                           },
                         //图例标题
-                          title:getYLabel( _reportObject.define.y.split(","))
-                          
+                          title:getYLabel( _reportObject.define.y.split(",")),
+                          /**
+                           * Highlight the given series item.
+                           * @param {Object} item Info about the item; same format as returned by #getItemForPoint.
+                           */
+                          highlightItem: function(item) {
+                              if (!item) {
+                                  return;
+                              }
+
+                              var me = this,
+                                  sprite = item.sprite,
+                                  opts = me.highlightCfg,
+                                  surface = me.chart.surface,
+                                  animate = me.chart.animate,
+                                  p, from, to, pi;
+                              animate=true;
+                              if (me.highlight === false || !sprite || sprite._highlighted) {
+                                  return;
+                              }
+                              //make sure we apply the stylesheet styles.
+                              Ext.applyIf(me.highlightCfg, me.highlightStyle.style || {});
+                              
+                              if (sprite._anim) {
+                                  sprite._anim.paused = true;
+                              }
+                              sprite._highlighted = true;
+                              if (!sprite._defaults) {
+                                  sprite._defaults = Ext.apply({}, sprite.attr);
+                                  from = {};
+                                  to = {};
+                                  for (p in opts) {
+                                      if (! (p in sprite._defaults)) {
+                                          sprite._defaults[p] = surface.attributeDefaults[surface.attributeMap[p]];
+                                      }
+                                      from[p] = sprite._defaults[p];
+                                      to[p] = opts[p];
+                                      if (Ext.isObject(opts[p])) {
+                                          from[p] = {};
+                                          to[p] = {};
+                                          Ext.apply(sprite._defaults[p], sprite.attr[p]);
+                                          Ext.apply(from[p], sprite._defaults[p]);
+                                          for (pi in sprite._defaults[p]) {
+                                              if (! (pi in opts[p])) {
+                                                  to[p][pi] = from[p][pi];
+                                              } else {
+                                                  to[p][pi] = opts[p][pi];
+                                              }
+                                          }
+                                          for (pi in opts[p]) {
+                                              if (! (pi in to[p])) {
+                                                  to[p][pi] = opts[p][pi];
+                                              }
+                                          }
+                                      }
+                                  }
+                                  sprite._from = from;
+                                  sprite._to = to;
+                                  sprite._endStyle = to;
+                              }
+                              
+                              if (animate) {
+                                  sprite._anim = new Ext.fx.Anim({
+                                      target: sprite,
+                                      from: sprite._from,
+                                      to: sprite._to,
+                                      duration: me.highlightDuration || 150
+                                  });
+                              } else {
+                                  sprite.setAttributes(sprite._to, true);
+                              }
+
+                              me.fireEvent('highlight', item);
+                          },
+
+                          /**
+                           * Un-highlight any existing highlights
+                           */
+                          unHighlightItem: function() {
+                              if (this.highlight === false || !this.items) {
+                                  return;
+                              }
+
+                              var me = this,
+                                  items = me.items,
+                                  len = items.length,
+                                  opts = me.highlightCfg,
+                                  animate = me.chart.animate,
+                                  i = 0,
+                                  obj, p, sprite;
+                              animate=true;
+                              for (; i < len; i++) {
+                                  if (!items[i]) {
+                                      continue;
+                                  }
+                                  sprite = items[i].sprite;
+                                  if (sprite && sprite._highlighted) {
+                                      if (sprite._anim) {
+                                          sprite._anim.paused = true;
+                                      }
+                                      obj = {};
+                                      for (p in opts) {
+                                          if (Ext.isObject(sprite._defaults[p])) {
+                                              obj[p] = {};
+                                              Ext.apply(obj[p], sprite._defaults[p]);
+                                          }
+                                          else {
+                                              obj[p] = sprite._defaults[p];
+                                          }
+                                      }
+                                      if (animate) {
+                                          //sprite._to = obj;
+                                          sprite._endStyle = obj;
+                                          sprite._anim = new Ext.fx.Anim({
+                                              target: sprite,
+                                              to: obj,
+                                              duration: me.highlightDuration || 150
+                                          });
+                                      }
+                                      else {
+                                          sprite.setAttributes(obj, true);
+                                      }
+                                      delete sprite._highlighted;
+                                      //delete sprite._defaults;
+                                  }
+                              }
+
+                              me.fireEvent('unhighlight');
+                          }
                          }   
                       
                       
